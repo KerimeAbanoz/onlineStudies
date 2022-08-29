ffaf8550571f6eb5c5626c71ef6e5e3e92ef400d
Kerime Abanoz <krmeabnz@gmail.com>
2022-08-29 20:28:33 +0300

    lesson started

---
+++ JavaScript/js09_arrays/arrays.js
@@ -0,0 +1,14 @@
+// ?=========================================================
+// ?                DIZILER (ARRAYS)
+// ?=========================================================
+
+console.log("***** ARRAYS ******");
+
+//* Dizi Tanimlama
+//* ---------------------------------------------------------
+
+const isimler = ["ahmet", "ismet", "can", "canan"];
+
+console.log(isimler);
+console.log(typeof isimler);
+console.log(isimler.length);
