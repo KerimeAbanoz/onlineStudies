fde17dd1221bc87c996190cbb0ac0c5bc7d0ee71
Kerime Abanoz <krmeabnz@gmail.com>
2022-08-31 20:31:10 +0300

    break

--- JavaScript/js10_array-iteration/1-loops.js
+++ JavaScript/js10_array-iteration/1-loops.js
@@ -4,7 +4,26 @@
 
 console.log("******* LOOPS IN ARRAYS ******");
 
+console.log("****** ITERATION **********");
 
+const dizi = [-5, 15, 22, -4, 45, 78];
+
+const negatifler = [];
+const pozitifler = [];
+
+const dizilereAyır = (n) => {
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] < 0) {
+      negatifler.push(n[i]);
+    } else {
+      pozitifler.push(n[i]);
+    }
+  }
+};
+dizilereAyır(dizi);
+
+console.log(negatifler);
+console.log(pozitifler);
 
 
 
