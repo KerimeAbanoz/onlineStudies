f23c3de7dd40a35bbd54dfc4d0513840b655efdc
Kerime Abanoz <krmeabnz@gmail.com>
2022-09-12 21:14:22 +0300

    lesson1 ended

--- JavaScript/js14_events/1-events-intro/events.js
+++ JavaScript/js14_events/1-events-intro/events.js
@@ -15,13 +15,30 @@ h1.onmouseover = function () {
 };
 
 h1.onmouseout = () => {
-    h1.style.color = "black";
-    h1.style.fontWeight = "900";
+  h1.style.color = "black";
+  h1.style.fontWeight = "900";
 };
 
 h1.addEventListener("click", () => {
-    alert("H1 pressed")
-})
+  alert("H1 pressed");
+});
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
+
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.querySelector("#input");
+
+  //     if(!input.value) {
+  //         alert("Please enter sometihng")
+  //     } else {
+  //         alert(`You entered ${input.value}`)
+  //     }
+  // })
+  !input.value
+    ? alert("Please enter sometihng")
+    : alert(`You entered ${input.value}`);
+
+  input.value = "";
+});
+
