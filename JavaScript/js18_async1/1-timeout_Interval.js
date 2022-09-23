57025b88efddb6e149db224c659773104633db7e
Kerime Abanoz <krmeabnz@gmail.com>
2022-09-23 17:08:26 +0300

    lesson sinished

--- JavaScript/js18_async1/1-timeout_Interval.js
+++ JavaScript/js18_async1/1-timeout_Interval.js
@@ -19,4 +19,70 @@
 //* Senkron
 //* ------------------------------------------------
 
-console.log("Timeout-Interval");
+// const bekle = (waitingTime) => {
+//   const startTime = new Date().getTime();
+//   while (new Date().getTime() < startTime + waitingTime);
+// };
+
+// console.log("Hello");
+// // alert("CW"); //! blocking code
+// console.time("timer");
+// bekle(3000); //! blocking code
+// console.timeEnd("timer");
+// console.log("FS12");
+
+//* Asenkron (setTimeout)
+//* -------------------------------------------------
+// console.log("timeout");
+// setTimeout(() => {
+//   console.log("Hi");
+// }, 2000);
+
+// setTimeout(() => {
+//   console.log("Hello");
+// }, 500);
+
+// console.log("timeout bitti");
+
+//* Asenkron (setInterval ve clearInterval)
+//* -------------------------------------------------
+
+// console.log("timer started");
+// let counter = 0;
+// const intervalId = setInterval(() => {
+//     console.log(++counter);
+//     if(counter>5){
+//         clearInterval(intervalId)
+        
+//     }
+// }, 1000);
+
+// console.log("timer stoped");
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
+// setTimeout(() => {
+//     console.log("john:Hi");
+//     setTimeout(() => {
+//       console.log("Sarah: Hello");
+//       setTimeout(() => {
+//         console.log("John: How Are you?");
+//         setTimeout(() => {
+//           console.log("Sarah:Fine and you?");
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+  
+  //? COZUMLER:
+  //?----------------------------------------------------
+  //* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+  //? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  //* 2- Promise,
+  //! 3- Fetch API (Promise'in basitlestirilmis hali),
+  //! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
+
