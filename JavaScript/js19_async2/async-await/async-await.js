7cebbcc5781b3ce874f7b0db975ee52f1b90bbf5
Kerime Abanoz <krmeabnz@gmail.com>
2022-09-29 19:58:09 +0300

    lesson to be continued

--- JavaScript/js19_async2/async-await/async-await.js
+++ JavaScript/js19_async2/async-await/async-await.js
@@ -18,8 +18,31 @@
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
 
-let isError = false;
-const API_KEY = "6e78d6a8e7274bcaa30783a1478193d4";
+// const getUsers = async () => {
+//const res = await fetch("......url......")
+
+//}
+
+const getNews = async function () {
+  const API_KEY = "6e78d6a8e7274bcaa30783a1478193d4";
+  const url =
+    "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Something went wrong ${res.status}`);
+    }
+    const data = await res.json();
+    // console.log(data.articles);
+    renderNews(data.articles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const renderNews = ()
+
+window.addEventListener("load", getNews);
 
 // let isError = false;
 
