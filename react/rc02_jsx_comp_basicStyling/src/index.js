1ae798d2df8781ada805e9ea7a78b95e51c85e74
Kerime Abanoz <krmeabnz@gmail.com>
2022-10-08 16:46:31 +0300

    component learned

--- react/rc02_jsx_comp_basicStyling/src/index.js
+++ react/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 import "./index.css";
+const a = 5;
+const Element = () => {
+  return (
+    <>
+      <h1>Hello React {a}</h1>
+      <p>merhaba</p>
+      <Child />
+      <Child />
+    </>
+  );
+};
+
+const Child = () => <p>I am child</p>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Element />);
