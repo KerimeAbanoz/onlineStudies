1cf5ff2a89094c5110d49de21c21bbea597e72f3
Kerime Abanoz <krmeabnz@gmail.com>
2022-10-08 16:47:34 +0300

    note added

--- react/rc02_jsx_comp_basicStyling/src/index.js
+++ react/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,21 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// import App from "./App";
+import App from "./App";
 import "./index.css";
-const a = 5;
-const Element = () => {
-  return (
-    <>
-      <h1>Hello React {a}</h1>
-      <p>merhaba</p>
-      <Child />
-      <Child />
-    </>
-  );
-};
 
-const Child = () => <p>I am child</p>;
+//! index componenti ReactDOM.createRoot() metodu ile
+//! public klasorunde yer alan index.html icersindeki id'si root
+//! olan elemana erisir.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Element />);
+root.render(<App />);
