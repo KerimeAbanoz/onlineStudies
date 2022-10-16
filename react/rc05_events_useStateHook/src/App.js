acfaf845cb2e869d6a1b39352949e7dce93011d8
Kerime Abanoz <krmeabnz@gmail.com>
2022-10-16 19:10:03 +0300

    classful comp started

--- react/rc05_events_useStateHook/src/App.js
+++ react/rc05_events_useStateHook/src/App.js
@@ -1,10 +1,12 @@
 import Events from "./components/1-events/Events";
+import Counter from "./components/2-classComponent/Counter";
 
 
 function App() {
   return (
     <div>
       <Events/>
+      <Counter/>
     </div>
   );
 }
--- react/rc05_events_useStateHook/src/components/2-classComponent/Counter.jsx
+++ react/rc05_events_useStateHook/src/components/2-classComponent/Counter.jsx
@@ -9,9 +9,36 @@
 //* constructor'in disinda state, setState() metodu ile degistilebilir.
 //* ====================================================================
 
+import { Component } from "react";
 
-  //! Yazmis oldugumuz metotlar default olarak classa baglanmaz.
-  //! Ancak, React built-in fonksiyonlari baglidir.
+class Counter extends Component {
+  constructor(props) {
+    super(props);
 
-  //* Bunun icin metotlarimizi ya constructor icerisinde baglamaliyiz ya da otomatik baglamayi saglayan arrow fonksiyonlarini kullanmaliyiz.
+    //! count state'ine başlangıç değeri atadık
+    this.state = {
+      count: 0,
+    };
+  }
 
+  render() {
+    return (
+      <div className="container text-center mt-4">
+        <h1>CASSFUL COMPONENTS</h1>
+        <h2 className="display-4 text-danger">COUNT:</h2>
+        <button className="btn btn-success">INC</button>
+
+        <button className="btn btn-dark">Clr</button>
+        <button className="btn btn-danger">DEC</button>
+
+      </div>
+    );
+  }
+}
+
+export default Counter;
+
+//! Yazmis oldugumuz metotlar default olarak classa baglanmaz.
+//! Ancak, React built-in fonksiyonlari baglidir.
+
+//* Bunun icin metotlarimizi ya constructor icerisinde baglamaliyiz ya da otomatik baglamayi saglayan arrow fonksiyonlarini kullanmaliyiz.
