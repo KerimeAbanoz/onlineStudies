4f6542052f467801f2e584b94e4eec8fee16950f
Kerime Abanoz <krmeabnz@gmail.com>
2022-10-18 15:40:14 +0300

    app deactivated

--- react/rc08_lifecycle-useEffect/src/App.js
+++ react/rc08_lifecycle-useEffect/src/App.js
@@ -14,7 +14,7 @@ function App() {
       {/* Short Circuit */}
       {/* {show && <LifeCycleMethods />} */}
 
-      {show && <UseEffectHook />}
+      {/* {show && <UseEffectHook />} */}
     </div>
   );
 }
